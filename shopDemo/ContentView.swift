c023527f6ed74f00ec21ab4ef7b5ae4971b14d11
李伟华 <q134134134@163.com>
2024-08-06 18:34:43 +0800

    git init

---
+++ shopDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shopDemo
+//
+//  Created by Lee Muzi on 2024/8/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shopDemo/shopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopDemoApp.swift
+//  shopDemo
+//
+//  Created by Lee Muzi on 2024/8/6.
+//
+
+import SwiftUI
+
+@main
+struct shopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
